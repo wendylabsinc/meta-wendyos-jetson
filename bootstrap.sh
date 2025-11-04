aeda0678e44859df55812ca4314ec9eb20fef936
Adrian Bularca <adrian.bularca@gmail.com>
2025-11-05 01:18:30 +0200

    Update build process
    Yocto build can run in a dedicated Docker container, which can be created
    either manually or automatically by the boostrap script.
    In case of a manual build, the 'dockerfile.config' file has to be modified manually
    to configure the host path(s) to be mounted in the Docker container.
    
    Signed-off-by: Adrian Bularca <adrian.bularca@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,21 +17,20 @@ WORK_DIR="$(pwd)"
 # PROJECT_DIR="${1:-${ROOT_DIR}}"
 PROJECT_DIR="${WORK_DIR}"
 LOG_FILE="${WORK_DIR}/yocto_setup.log"
+META_EDGEOS="${HOME_DIR}"
+DOCKER_WORK_DIR="/home/dev/edgeos"
+
 
 YOCTO_BRANCH="scarthgap"
 YOCTO_BUILD_DIR="build"
 
-EDGEOS_URL="git@github.com:mihai-chiorean/meta-edgeos-jetson.git"
-EDGEOS_BRANCH="${EDGEOS_BRANCH:-}"
-
-
-[[ -z "${EDGEOS_BRANCH}" ]] && {
-    prog="$(basename $0)"
-    printf "meta-edgeos branch must be provided!\n"
-    printf "Run:\n"
-    printf "   EDGEOS_BRANCH=<branch> <path>/${prog}\n"
-    exit 1
+cleanup() {
+    # preserve original exit code
+    rc=$?
+    cd -- "${WORK_DIR}" || true
+    exit "$rc"
 }
+trap cleanup EXIT
 
 declare -Ar repos=(
     [0]="1|git://git.yoctoproject.org/poky.git||${YOCTO_BRANCH}"
@@ -40,7 +39,6 @@ declare -Ar repos=(
     [3]="1|https://github.com/OE4T/meta-tegra-community||${YOCTO_BRANCH}"
     [5]="1|https://github.com/mendersoftware/meta-mender.git||${YOCTO_BRANCH}"
     [6]="1|https://github.com/mendersoftware/meta-mender-community.git||${YOCTO_BRANCH}"
-    [4]="1|${EDGEOS_URL}|meta-edgeos|${EDGEOS_BRANCH}"
 )
 
 
@@ -105,6 +103,36 @@ function clone_repos() {
     done
 }
 
+copy_dir() {
+    local src="$1"
+    local dst="$2"
+
+    if [ -z "$src" ] || [ -z "$dst" ]; then
+        echo "Usage: copy_dir <source_dir> <dest_dir>" >&2
+        return 2
+    fi
+
+    if [ ! -d "$src" ]; then
+        echo "Source is not a directory: $src" >&2
+        return 1
+    fi
+
+    # Ensure destination exists
+    mkdir -p -- "$dst" || return $?
+
+    if command -v ditto >/dev/null 2>&1; then
+        # Best on macOS: preserves permissions, ACLs, xattrs, symlinks
+        ditto "$src" "$dst"
+    elif command -v rsync >/dev/null 2>&1; then
+        # Cross-platform: preserves perms, times, symlinks, devices, etc.
+        # Trailing slashes copy contents of src into dst
+        rsync -aH -- "$src"/ "$dst"/
+    else
+        # POSIX fallback (may not keep ACLs/xattrs)
+        cp -Rpv -- "$src"/. "$dst"/
+    fi
+}
+
 [[ ! -d "${PROJECT_DIR}" ]] && {
     mkdir -p "${PROJECT_DIR}"
 }
@@ -120,17 +148,24 @@ clone_repos || {
     exit 1
 }
 
+image_name=$(basename "${WORK_DIR}")
+
 cd "${PROJECT_DIR}"
 if [[ ! -d "${YOCTO_BUILD_DIR}" ]]
 then
     printf "\nPrepare the Yocto build environment...\n"
     mkdir -p "${YOCTO_BUILD_DIR}/conf"
 
-    path="${PROJECT_DIR}/repos"
-    cp "${path}/meta-edgeos/conf/template/bblayers.conf" "./${YOCTO_BUILD_DIR}/conf"
-    cp "${path}/meta-edgeos/conf/template/local.conf" "./${YOCTO_BUILD_DIR}/conf"
+    cp "${META_EDGEOS}/conf/template/bblayers.conf" "./${YOCTO_BUILD_DIR}/conf"
+    cp "${META_EDGEOS}/conf/template/local.conf" "./${YOCTO_BUILD_DIR}/conf"
 
-    sed -i.bak "s|%PATH%|${path}|g" "./${YOCTO_BUILD_DIR}/conf/bblayers.conf"
+    # tmp=$(basename "${WORK_DIR}")
+    tmp="${DOCKER_WORK_DIR}/${image_name}/repos"
+    sed -i.bak "s|%PATH%|${tmp}|g" "./${YOCTO_BUILD_DIR}/conf/bblayers.conf"
+
+    tmp=$(basename "${META_EDGEOS}")
+    tmp="${DOCKER_WORK_DIR}/${tmp}"
+    sed -i.bak "s|%PATH_EDGEOS%|${tmp}|g" "./${YOCTO_BUILD_DIR}/conf/bblayers.conf"
 else
     printf "Yocto build directory already exists!\n"
 fi
@@ -138,9 +173,29 @@ fi
 printf "\nDirectory structure:\n"
 tree -d -L 2 #--charset=ascii
 
-    cat <<EOF
+# prepare Docker image
+printf "\nCreate docker image...\n"
+docker_path="${PROJECT_DIR}/../docker"
+mkdir -p "${docker_path}"
+copy_dir "${META_EDGEOS}/scripts/docker" "${docker_path}"
+
+# tmp="${PROJECT_DIR}/.."
+tmp="$(cd -P -- "${ROJECT_DIR}/.." && pwd -P)"
+sed -i.bak "s|%EDGEOS_DIR%|${tmp}|g" "${docker_path}/dockerfile.config"
+
+cd "${WORK_DIR}/../docker"
+./docker-util.sh create
+
+cd "${WORK_DIR}"
+cat <<EOF
 
 Run the command(s):
+   # run the docker container
+   cd ../docker
+   ./docker-util.sh run
+
+   # (on container)
+   cd ./edgeos/${image_name}
    . ./repos/poky/oe-init-build-env ${YOCTO_BUILD_DIR}
    bitbake edgeos-image
 
---
+++ scripts/docker/docker-util.sh
@@ -0,0 +1,306 @@
+#!/usr/bin/env bash
+
+#
+# ./docker-util.sh --create --config=dockerfile --image=<image_name>
+# ./docker-util.sh --run --image=<image_name> --in="output" --out="/home/phoenix/yocto"
+# ./docker-util.sh --remove --image=<image_name>
+#
+
+set -euo pipefail
+
+# colored console support
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+CON_SWITCH="\033["
+CON_RESET="${CON_SWITCH}0m"
+# CON_RED="${CON_SWITCH}0;31m"
+CON_LRED="${CON_SWITCH}1;31m"
+# CON_GREEN="${CON_SWITCH}0;32m"
+# CON_LGREEN="${CON_SWITCH}1;32m"
+# CON_ORANGE="${CON_SWITCH}0;33m"
+# CON_YELLOW="${CON_SWITCH}1;33m"
+# CON_BLUE="${CON_SWITCH}0;34m"
+# CON_LBLUE="${CON_SWITCH}1;34m"
+# CON_LCYAN="${CON_SWITCH}1;36m"
+# CON_GRAY="${CON_SWITCH}1;30m"
+# CON_LGRAY="${CON_SWITCH}0;37m"
+# CON_WHITE="${CON_SWITCH}1;37m"
+
+DOCKER_ARGS=""
+
+###
+function parse_config() {
+    local config_file="$1"
+    local src
+    local dest
+    local rights
+
+    while read -r line; do
+        [[ "$line" =~ ^#.*$ ]] && {
+            #ignore comments
+            continue
+        }
+
+        [[ -z "$line" ]] && {
+            #ignore empty lines
+            continue
+        }
+
+        src=$(echo "${line}" | cut -d':' -f 1)
+        src=$(eval echo "${src}")
+
+        dest=$(echo "${line}" | cut -s -d':' -f 2)
+        dest=$(eval echo "${dest}")
+
+        rights=$(echo "${line}" | cut -s -d':' -f 3)
+        rights=$(eval echo "${rights}")
+
+        # printf "Host: '%s'\n" "${src}"
+        # printf "Docker: '%s'\n" "${dest}"
+        # [ ! -d "${src}" ] && {
+        #     mkdir -p "${src}"
+        # }
+
+        DOCKER_ARGS="${DOCKER_ARGS} -v ${src}:${dest}"
+        [[ -n "${rights}" ]] && {
+            DOCKER_ARGS="${DOCKER_ARGS}:${rights}"
+        }
+    done < "${config_file}"
+}
+
+# folder where the script is located
+HOME_DIR="$(realpath "${0%/*}")"
+
+# folder from which the script was called
+WORK_DIR="$(pwd)"
+
+declare -A opts=(
+    [create]=0
+    [remove]=0
+)
+
+DOCKER_BASE="${DOCKER_BASE:-ubuntu:24.04}"
+DOCKER_NAME=""
+# DOCKER_REPO="edgeos"
+DOCKER_REPO="${DOCKER_REPO:-edgeos-test}"
+# DOCKER_TAG="latest"
+DOCKER_TAG="${DOCKER_TAG:-scarthgap}"
+DOCKER_CONFIG=""
+DOCKER_ENV="${WORK_DIR}/environment"
+DOCKER_USER="dev"
+DOCKER_HOST="yocto"
+
+cleanup() {
+    # preserve original exit code
+    rc=$?
+    cd -- "${WORK_DIR}" || true
+    exit "$rc"
+}
+trap cleanup EXIT
+
+##
+# display help
+usage() {
+    cat <<EOF
+
+Usage:
+  $(basename "$0") [options] command
+
+Options:
+  --name=<name>         Docker container name (optional)
+  --repo=<name>         Docker repository name [${DOCKER_REPO}]
+  --tag=<name>          Docker tag name [${DOCKER_TAG}]
+  --user                User name running on Docker container [${DOCKER_USER}]
+  --host                Docker host name [${DOCKER_HOST}]
+
+Command(s):
+  create        Build Docker image
+  run           Run the docker container
+  remove        Remove Docker image
+
+[Examples]
+(using the default configuration)
+
+Create an image:
+  $(basename $0) create
+
+Run an image:
+  $(basename $0) run
+
+EOF
+}
+
+### main
+[ $# -eq 0 ] && {
+    usage
+    exit 0
+}
+
+cmd_parsed=0
+
+# parse command line arguments...
+while [ "$#" -gt 0 ]; do
+    arg=$1
+    shift
+
+    [[ 1 -eq "${cmd_parsed}" ]] && {
+        # in case multiple commands are provided, only the first one is considered
+        break
+    }
+
+    # additional argument processing...
+    case ${arg} in
+    create)
+        opts[create]=1
+        cmd_parsed=1
+        ;;
+
+    run)
+        opts[create]=0
+        cmd_parsed=1
+        ;;
+
+    remove)
+        opts[remove]=1
+        cmd_parsed=1
+        ;;
+
+    --name=*)
+        if [[ "${arg}" == *"="* ]]
+        then
+            tmp=${arg##*=}
+            [ ! -z "${tmp}" ] && {
+                DOCKER_NAME="${tmp}"
+            }
+        fi
+        ;;
+
+    --repo=*)
+        if [[ "${arg}" == *"="* ]]
+        then
+            tmp=${arg##*=}
+            [ ! -z "${tmp}" ] && {
+                DOCKER_REPO="${tmp}"
+            }
+        fi
+        ;;
+
+    --tag=*)
+        if [[ "${arg}" == *"="* ]]
+        then
+            tmp=${arg##*=}
+            [ ! -z "${tmp}" ] && {
+                DOCKER_TAG="${tmp}"
+            }
+        fi
+        ;;
+
+    --user=*)
+        if [[ "${arg}" == *"="* ]]
+        then
+            tmp=${arg##*=}
+            [ ! -z "${tmp}" ] && {
+                DOCKER_USER="${tmp}"
+            }
+        fi
+        ;;
+
+    --host=*)
+        if [[ "${arg}" == *"="* ]]
+        then
+            tmp=${arg##*=}
+            [ ! -z "${tmp}" ] && {
+                DOCKER_HOST="${tmp}"
+            }
+        fi
+        ;;
+
+    h|--help)
+        usage
+        exit 0
+        ;;
+
+    *)
+        printf "error: unknown argument '%s'\n" "${arg}"
+        exit 1
+        ;;
+    esac
+done
+
+[ -z "${DOCKER_REPO}" ] && {
+    printf "${CON_LRED}Error${CON_RESET}: Docker image name not provided\n"
+    exit 1
+}
+
+[ 1 -eq "${opts[remove]}" ] && {
+    printf "Remove Docker image... (%s)\n" "${DOCKER_REPO}"
+    docker image rm "${DOCKER_REPO}"
+    exit $?
+}
+
+if [[ 1 -eq "${opts[create]}" ]]
+then
+    DOCKER_CONFIG="${WORK_DIR}/dockerfile"
+
+    [ ! -f "${DOCKER_CONFIG}" ] && {
+        printf "${CON_LRED}Error${CON_RESET}: Docker file not found (%s)\n" "${DOCKER_CONFIG}"
+        exit 1
+    }
+
+    [ -n "${DOCKER_NAME}" ] && {
+        DOCKER_ARGS="${DOCKER_ARGS} --name ${DOCKER_NAME}"
+    }
+
+    printf "Generate Docker image...\n"
+    printf "  Docker base image... (%s)\n" "${DOCKER_BASE}"
+    printf "  Docker file... (%s)\n" "${DOCKER_CONFIG}"
+    printf "  Docker repository... (%s)\n" "${DOCKER_REPO}"
+    printf "  Docker tag name... (%s)\n" "${DOCKER_TAG}"
+    docker build \
+        --network=host \
+        --file "${DOCKER_CONFIG}" \
+        --no-cache \
+        --build-arg "host_uid=$(id -u)" \
+        --build-arg "host_gid=$(id -g)" \
+        --build-arg "user_name=${DOCKER_USER}" \
+        --build-arg "DOCKER_TAG=${DOCKER_TAG}" \
+        --build-arg "BASE_IMAGE=${DOCKER_BASE}" \
+        --tag "${DOCKER_REPO}:${DOCKER_TAG}" \
+        .
+    exit $?
+else
+    printf "Run Docker image... (%s)\n" "${DOCKER_REPO}"
+
+    DOCKER_CONFIG="${WORK_DIR}/dockerfile.config"
+    [ -f "${DOCKER_CONFIG}" ] && {
+        printf "Read configuration file '%s'...\n" "${DOCKER_CONFIG}"
+        parse_config "${DOCKER_CONFIG}"
+    }
+
+    [[ -f "${DOCKER_ENV}" ]] && {
+        DOCKER_ARGS="${DOCKER_ARGS} --env-file ${DOCKER_ENV}"
+    }
+
+    docker run \
+        --interactive \
+        --tty \
+        --rm \
+        -e DISPLAY=$DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        --network host \
+        --privileged \
+        -e "TERM=xterm-256color" \
+        -e "LANG=C.UTF-8" \
+        -e "DOCKER_TAG=${DOCKER_TAG}" \
+        -u "$(id -u):$(id -g)" \
+        --hostname ${DOCKER_HOST} \
+        ${DOCKER_ARGS} \
+        "${DOCKER_REPO}:${DOCKER_TAG}"
+    exit $?
+fi
